c5c86bf1e5baa54daeeb5996d61bc0b9f85a4842
Sandeep Vattapparambil <sandeepv68@gmail.com>
2017-06-20 21:39:37 +0530

    Hello PWA

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+
+  swPrecache.write(path.join('sw.js'), {
+    staticFileGlobs: [
+      '**.**',
+      'css/**.css',
+      'js/**.js',
+      'fonts/**/**.*'
+    ]
+  }, callback);
+});
