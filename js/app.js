263594e4be3f054afe484b441120d0e8f9e9e2aa
Sandeep Vattapparambil <sandeepv68@gmail.com>
2017-06-24 20:56:42 +0530

    ui updates

--- js/app.js
+++ js/app.js
@@ -13,8 +13,16 @@ if ('serviceWorker' in navigator) {
 //Materializecss initializations
 $(".button-collapse").sideNav();
 $('.collapsible').collapsible();
+$('.tooltipped').tooltip({delay: 50});
 
 //Main hire me button scroll down
+$("#goto_top").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#top").offset().top - 64
+  }, 2000);
+});
+
+//Go to top
 $("#hire_me").click(function() {
   $('html, body').animate({
     scrollTop: $("#card_6").offset().top
@@ -124,33 +132,4 @@ $(document).ready(function() {
       expires: 7
     });
   }
-  /*
-  //Create a cookie, valid across the entire site:
-  Cookies.set('name', 'value');
-  //Create a cookie that expires 7 days from now, valid across the entire site:
-  Cookies.set('name', 'value', {
-    expires: 7
-  });
-  //Create an expiring cookie, valid to the path of the current page:
-  Cookies.set('name', 'value', {
-    expires: 7,
-    path: ''
-  });
-  ///Read cookie:
-  Cookies.get('name'); // => 'value'
-  Cookies.get('nothing'); // => undefined
-  //Read all visible cookies:
-  Cookies.get(); // => { name: 'value' }
-  //Delete cookie:
-  Cookies.remove('name');
-  //Delete a cookie valid to the path of the current page:
-  Cookies.set('name', 'value', {
-    path: ''
-  });
-  Cookies.remove('name'); // fail!
-  Cookies.remove('name', {
-    path: ''
-  }); // removed!
-*/
-
 });
