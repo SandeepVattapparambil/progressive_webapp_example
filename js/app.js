87bf712910e363411678c2facdee1571b93f864a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2017-06-24 20:22:14 +0530

    cookie updates

--- js/app.js
+++ js/app.js
@@ -104,3 +104,46 @@ function initMap() {
     map: map
   });
 }
+
+//Cookie ctrl
+$(document).ready(function() {
+  function hasCookies() {
+    return (navigator.cookieEnabled);
+  }
+  var cookie_check = hasCookies();
+  if(cookie_check){
+    console.log('Cookie enabled!');
+  }
+  else{
+    Materialize.toast('<span class="red-text"><strong>Please enable cookies!</strong></span>', 4000);
+  }
+  /*
+  //Create a cookie, valid across the entire site:
+  Cookies.set('name', 'value');
+  //Create a cookie that expires 7 days from now, valid across the entire site:
+  Cookies.set('name', 'value', {
+    expires: 7
+  });
+  //Create an expiring cookie, valid to the path of the current page:
+  Cookies.set('name', 'value', {
+    expires: 7,
+    path: ''
+  });
+  ///Read cookie:
+  Cookies.get('name'); // => 'value'
+  Cookies.get('nothing'); // => undefined
+  //Read all visible cookies:
+  Cookies.get(); // => { name: 'value' }
+  //Delete cookie:
+  Cookies.remove('name');
+  //Delete a cookie valid to the path of the current page:
+  Cookies.set('name', 'value', {
+    path: ''
+  });
+  Cookies.remove('name'); // fail!
+  Cookies.remove('name', {
+    path: ''
+  }); // removed!
+*/
+
+});
